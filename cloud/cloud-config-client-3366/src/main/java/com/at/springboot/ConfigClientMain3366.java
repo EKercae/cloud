3d9d1968b85fae31009543c930efde2f04550029
EKercae <ekercae@foxmail.com>
2021-05-28 16:33:26 +0800

    2021528

---
+++ cloud/cloud-config-client-3366/src/main/java/com/at/springboot/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.at.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author user
+ * @create 2021-05-27 18:45
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
---
+++ cloud/cloud-config-client-3366/src/main/java/com/at/springboot/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.at.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author user
+ * @create 2021-05-27 18:46
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @RequestMapping("/configInfo")
+    public String configInfo() {
+        return serverPort + "-------" + configInfo;
+    }
+}
